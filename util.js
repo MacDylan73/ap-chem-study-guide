29df68252da97d7e379d4a7980953998140bd010
Dylan Folscroft <dfolscroft805@gmail.com>
2025-08-16 16:00:15 -0400

    Update util.js

--- util.js
+++ util.js
@@ -1,1 +1,19 @@
-ent
+// Toggle for subunit dropdowns
+document.querySelectorAll('.subunit-header').forEach(button => {
+    button.addEventListener('click', () => {
+      button.classList.toggle('active');
+    });
+  });
+
+// Toggle for sidebar menu
+  function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.style.transform = sidebar.style.transform === 'translateX(0%)' ? 'translateX(-100%)' : 'translateX(0%)';
+  }
+
+// Toggle Light/Dark Mode Themes
+  function toggleTheme() {
+    document.body.classList.toggle('dark-mode');
+    const icon = document.getElementById('theme-icon');
+    icon.textContent = document.body.classList.contains('dark-mode') ? '🌙' : '🌞';
+  }
